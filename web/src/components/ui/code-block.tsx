035a14e71666e4fdf46daa6c3385148c6beee3b1
ext-dgoncalves <dgoncalves@pdmfc.com>
2025-11-27 10:29:29 +0000

    feat: create web

---
+++ web/src/components/ui/code-block.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState, type ComponentProps } from "react"
+import { codeToHtml } from 'shiki'
+import { twMerge } from "tailwind-merge"
+
+interface CodeBlockProps extends ComponentProps<'div'> {
+  code: string
+  language?: string
+}
+
+export function CodeBlock({ 
+  className, 
+  code, 
+  language = 'json', 
+  ...props 
+}: CodeBlockProps) {
+  const [parsedCode, setParsedCode] = useState('')
+
+  useEffect(() => {
+    if (!code) return
+
+    if (code) {
+      codeToHtml(code, {lang: language, theme: 'vesper'}).then((parsed) => {
+        setParsedCode(parsed)
+      })
+    }
+  }, [code, language])
+
+  return (
+    <div className={twMerge('relative rounded-lg border border-zinc-700 overflow-x-auto', className)} {...props}>
+      <div 
+        dangerouslySetInnerHTML={{ __html: parsedCode }}
+        className="[&_pre]:p-4"
+      />
+    </div>
+  )
+}
+  
